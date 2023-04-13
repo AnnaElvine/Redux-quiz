d2d06a0b79cfc24d8202fececbbfd7d4731fa13a
Emilia Saberski <emilia@saberski.com>
2023-04-13 11:48:17 +0200

    Trying to show images as question and option

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,22 +1,40 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { quiz } from 'reducers/quiz'
+import styled from 'styled-components'
+
+const ImgButton = styled.img`
+    width: 100px;
+    cursor: pointer;
+    `
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
 
+  const dispatch = useDispatch(quiz.actions.submitAnswer);
+
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
+  }
+  const onButtonClick = () => {
+    dispatch(quiz.actions.submitAnswer())
+    dispatch(quiz.actions.goToNextQuestion())
   }
 
   return (
     <div>
-      <h1>Question: {question.questionText}</h1>
+      <img src={question.questionImg} alt="" />
       {question.options.map((option) => {
         return (
-          <button
-            type="button">
-            {option}
-          </button>
+          <ImgButton
+            src={option}
+            alt="text"
+            onClick={onButtonClick} />
+          // <ImgButton
+          //   type="button"
+          //   onClick={onButtonClick}>
+          //   {option}
+          // </ImgButton>
         )
       })}
     </div>
