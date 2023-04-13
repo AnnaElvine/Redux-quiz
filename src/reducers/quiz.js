4cd582cbffe6bb7e42e8ecd87b909640dcdd3a3d
Andrea <andrea@andreas-air.guest.stockholm.se>
2023-04-13 12:48:33 +0200

    fixed spelling

--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -7,7 +7,7 @@ import Pink from '../components/images/Pink.png'
 
 // Change these to your own questions!
 const questions = [
-  { id: 1, questionImg: `${Purple}`, options: ['RED', 'GREEN', 'PURPLE', 'ORANGE'], correctAnswerIndex: 0 },
+  { id: 1, questionImg: `${Purple}`, options: ['RED', 'GREEN', 'PURPLE', 'ORANGE'], correctAnswerIndex: 3 },
   { id: 2, questionImg: `${Green}`, options: ['2012', '2014', '2016', '2018'], correctAnswerIndex: 2 },
   { id: 3, questionImg: `${Orange}`, options: ['2012', '2014', '2016', '2018'], correctAnswerIndex: 3 },
   { id: 4, questionImg: `${Brown}`, options: ['2012', '2014', '2016', '2018'], correctAnswerIndex: 4 },
