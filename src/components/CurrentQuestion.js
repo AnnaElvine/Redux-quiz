13200947ff36c30d58b59fbd7fcd5be5c1471fe6
Andrea <andrea@andreas-air.guest.stockholm.se>
2023-04-13 13:54:04 +0200

    added buttons

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { quiz } from 'reducers/quiz'
-import styled from 'styled-components'
-
-const ImgButton = styled.img`
-    width: 100px;
-    cursor: pointer;
-    `
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
   const dispatch = useDispatch();
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
   const onButtonClick = () => {
-    dispatch(quiz.actions.submitAnswer())
-    dispatch(quiz.actions.goToNextQuestion())
+    dispatch(quiz.actions.submitAnswer());
+    dispatch(quiz.actions.goToNextQuestion());
   }
 
   return (
@@ -26,15 +19,11 @@ export const CurrentQuestion = () => {
       <img src={question.questionImg} alt="" />
       {question.options.map((option) => {
         return (
-          <ImgButton
-            src={option}
-            alt="text"
-            onClick={onButtonClick} />
-          // <ImgButton
-          //   type="button"
-          //   onClick={onButtonClick}>
-          //   {option}
-          // </ImgButton>
+          <button
+            type="button"
+            onClick={onButtonClick}>
+            {option}
+          </button>
         )
       })}
     </div>
