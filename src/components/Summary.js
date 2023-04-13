58df0724f33b071034a2744d6e26e2e3d09ff10f
Lina Adamsson <studio@adamsson.com>
2023-04-13 22:33:43 +0200

    Added summary info and confetti effect to the Summary component

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { quiz } from 'reducers/quiz';
-import { useDispatch } from 'react-redux';
+import Confetti from 'react-confetti-explosion';
+import { useSelector, useDispatch } from 'react-redux';
 
 export const Summary = () => {
+  const answers = useSelector((state) => state.quiz.answers);
+  const correctAnswers = answers.filter((answer) => answer.isCorrect).length;
   const dispatch = useDispatch()
   return (
-    <button
-      type="button"
-      onClick={() => dispatch(quiz.actions.restart())}>
+    <>
+      <Confetti />
+      You got {correctAnswers} correct answers out of 5
+      <button
+        type="button"
+        onClick={() => dispatch(quiz.actions.restart())}>
         Restart
-    </button>
+      </button>
+    </>
   )
 }
