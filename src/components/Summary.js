a8ccf00b2b4884dd07c2f07e2aea221b3c1fb604
Andrea <andrea@Andreas-Air.lan>
2023-04-14 02:23:07 +0200

    summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { quiz } from 'reducers/quiz';
-/* import Confetti from 'react-confetti-explosion'; */
+import styled from 'styled-components';
+import Confetti from 'react-confetti-explosion';
 import { useSelector, useDispatch } from 'react-redux';
+
+const SummaryContainer = styled.div`
+display: flex;
+flex-direction: column;
+background: white;
+height: 500px;
+border-radius: 20px;
+margin: 35px;
+`
+
+const RainbowImage = styled.img`
+width: 100%;
+height: 100px;
+`
+const SummaryText = styled.h1`
+font-size: 18px;
+`
+
+const Button = styled.button`
+display: flex;
+align-self: center;
+background-color: pink;
+font-size: 15px;
+width: 150px;
+height: 40px;
+border-style: none;
+border-radius: 15px; 
+`
 
 export const Summary = () => {
   const answers = useSelector((state) => state.quiz.answers);
@@ -9,14 +38,18 @@ export const Summary = () => {
   const dispatch = useDispatch()
   return (
     <>
-      You got {correctAnswers} correct answers out of 5
-      <button
-        type="button"
-        onClick={() => dispatch(quiz.actions.restart())}>
+      <Confetti />
+      <SummaryContainer>
+        <RainbowImage src="./images/PinkSix.png" alt=""/>
+        <SummaryText>
+          <h1>You got {correctAnswers} correct answers out of 5</h1>
+        </SummaryText>
+        <Button
+          type="button"
+          onClick={() => dispatch(quiz.actions.restart())}>
         Restart
-      </button>
+        </Button>
+      </SummaryContainer>
     </>
   )
 }
-
-/* <Confetti /> */
