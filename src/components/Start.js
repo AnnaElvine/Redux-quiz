91ec60853d57118e7e3e89a63c7b07f3dcbc521a
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-04-14 18:36:00 +0200

    .

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-duplicates */
+/* eslint-disable no-tabs */
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { keyframes } from 'styled-components';
 import styled from 'styled-components'
 
 const Button = styled.button`
-background-color: #6fc16f;
+background-color: yello;
 font-size: 15px;
 width: 100px;
 height: 100px;
@@ -15,8 +18,7 @@ box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
   outline: none;
   transition: all 0.3s ease 0s;
   &:hover {
-    /* background-color: lightblue; */
-    transform: translateY(-7px);
+  transform: translateY(-7px);
   }
 `
 const StyledParagraph = styled.p`
@@ -25,29 +27,103 @@ const StyledParagraph = styled.p`
   margin: 5px;
   padding: 20px;
   text-align: center;
+  cursor: default;
 `
+const balanceAnimation = keyframes`
+  0%, 100% {
+    transform: rotate(0deg);
+  }
+  30%, 60% {
+    transform: rotate(-45deg);
+  }
+`;
+
+const shrinkJumpAnimation = keyframes`
+  10%, 35% {
+    transform: scale(2, .2) translate(0, 0);
+  }
+
+  45%, 50% {
+    transform: scale(1) translate(0, -150px);
+  }
+
+  80% {
+    transform: scale(1) translate(0, 0);
+  }
+`;
+const StyledSpan = styled.span`
+  color: purple;
+  font-weight: bold;
+  font-size: 35px;
+  &:hover {
+    color: green;
+  }
+  .word span {
+    display: inline-block;
+    font-size: 100px;
+    line-height: 0.8;
+  }
+  .word span:nth-child(1).active {
+    animation: ${balanceAnimation} 1.5s ease-out;
+    transform-origin: bottom left;
+  }
+  .word span:nth-child(2).active {
+    animation: ${shrinkJumpAnimation} 1s ease-in-out;
+    transform-origin: bottom center;
+  }
+  `
+
 const StartContainer = styled.div`
 display: flex;
 flex-direction: column;
-/*background: white; */
-/* border-radius: 20px; */
 margin: 35px;
 height: 500px;
 align-items: center;
 padding: 10px;
 margin: 35px;
-justify-content: center
+justify-content: center;
+`
+
+const Sun = styled.div`
+width: 40rem;
+  height: 40rem;
+  margin: 3rem auto;
+  position: relative;
+  background: radial-gradient(transparent 50%, white), radial-gradient(yellow, transparent 70%);
+  -webkit-mask-image: radial-gradient(rgba(0, 0, 0, 1.0) 40%, transparent 65%);
+  mask-image: radial-gradient(rgba(0, 0, 0, 1.0) 40%, transparent 65%);
+  border-radius: 50%;
 `
 
 export const Start = () => {
   return (
     <StartContainer>
-      <StyledParagraph>Nu ska vi blanda färg, gissa vad det blir</StyledParagraph>
+      <StyledParagraph>
+        Nu ska vi blanda lite
+        <StyledSpan>
+          <div className="word">
+            <span className="active">f</span>
+            <span className="word">
+              <span className="active">ä</span>
+            </span>
+            <span className="word">
+              <span className="active">r</span>
+            </span>
+            <span className="word">
+              <span className="active">g</span>
+            </span>
+          </div>
+        </StyledSpan>
+         gissa vad det blir...
+      </StyledParagraph>
+      <Sun>
+        <div className="sun" />
+      </Sun>
       <Link to="/quiz">
         <Button>
-        Start
+          Start
         </Button>
       </Link>
     </StartContainer>
-  )
-}
+  );
+};
